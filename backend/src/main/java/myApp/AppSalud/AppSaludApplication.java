c7cfe90960a99ac646964bee7a2a46acbb07bba9
Sofia Raia <sofiaraia246@outlook.es>
2023-05-22 20:38:56 -0300

    creamos carpeta backend

---
+++ backend/src/main/java/myApp/AppSalud/AppSaludApplication.java
@@ -0,0 +1,13 @@
+package myApp.AppSalud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppSaludApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppSaludApplication.class, args);
+	}
+
+}
